638d4b08e1559c7ef0aa5da953047bdf82f1d903
MaxwellJoneZ <axel.pfister@gmail.com>
2026-06-02 16:34:44 +0200

    Move KidzJadeTiers to new package: com.maxwelljonez.silentgearjadetiers

---
+++ src/main/java/com/maxwelljonez/silentgearjadetiers/KidzJadeTiers.java
@@ -0,0 +1,8 @@
+package com.maxwelljonez.silentgearjadetiers;
+
+import net.neoforged.fml.common.Mod;
+
+@Mod(KidzJadeTiers.MODID)
+public final class KidzJadeTiers {
+    public static final String MODID = "kidz_jade_tiers";
+}
