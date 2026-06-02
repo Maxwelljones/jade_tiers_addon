896bdf82a56e72ad569aa0d78bc746fa6a219cb5
MaxwellJoneZ <axel.pfister@gmail.com>
2026-06-02 16:38:22 +0200

    Delete src/main/java/com/kidz5/jadetiers/KidzJadeTiers.java

--- src/main/java/com/kidz5/jadetiers/KidzJadeTiers.java
+++
@@ -1,8 +0,0 @@
-package com.kidz5.jadetiers;
-
-import net.neoforged.fml.common.Mod;
-
-@Mod(KidzJadeTiers.MODID)
-public final class KidzJadeTiers {
-    public static final String MODID = "kidz_jade_tiers";
-}
