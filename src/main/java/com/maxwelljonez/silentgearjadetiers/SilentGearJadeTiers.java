17e0c0a29d9edfa966c659118ac5a3a351fb34c2
MaxwellJoneZ <axel.pfister@gmail.com>
2026-06-02 16:58:17 +0200

    Add constructor to SilentGearJadeTiers class

--- src/main/java/com/maxwelljonez/silentgearjadetiers/SilentGearJadeTiers.java
+++ src/main/java/com/maxwelljonez/silentgearjadetiers/SilentGearJadeTiers.java
@@ -1,8 +1,15 @@
 package com.maxwelljonez.silentgearjadetiers;
 
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.config.ModConfig;
 
 @Mod(SilentGearJadeTiers.MODID)
 public final class SilentGearJadeTiers {
     public static final String MODID = "silentgear_jade_tiers";
+
+    public SilentGearJadeTiers(IEventBus modEventBus, ModContainer modContainer) {
+        modContainer.registerConfig(ModConfig.Type.COMMON, SilentGearJadeTiersConfig.SPEC);
+    }
 }
